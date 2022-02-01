c74475e5af7c07f9ed6d23dc5ebd00c97cf55296
ReNiFeD <definer@gpswis.com>
2022-02-01 11:21:35 -0800

    Initial Commit: Basic Structure

---
+++ src/main/java/com/example/LogInSystem/User.java
@@ -0,0 +1,72 @@
+package com.example.LogInSystem;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", length = 25, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 25, nullable = false)
+    private String lastName;
+
+    @Column(length = 200, nullable = false)
+    private String email;
+
+    @Column(length = 25, unique = true, nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/com/example/LogInSystem/UserRepositoryTests.java
@@ -0,0 +1,4 @@
+package com.example.LogInSystem;
+
+public class UserRepositoryTests {
+}
